47c2ba6d7471b8d6e27db7a5e85019ed4cf71119
Simone823 <simone.daglio823@outlook.it>
2022-03-07 17:05:00 +0100

    bonus hero

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,30 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <MyTitle/>
+    <MyHero/>
   </div>
 </template>
 
 <script>
 import MyTitle from './components/MyTitle.vue'
+import MyHero from './components/MyHero.vue'
 
 export default {
   name: 'App',
   components: {
     MyTitle,
+    MyHero,
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 body {
   background-color: #2c3e50;
 }
---
+++ src/components/MyHero.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="jumbotron">
+      <h1>My hero content</h1>
+    </div>
+</template>
+
+
+
+<style scoped>
+.jumbotron{
+  background-image: linear-gradient(
+    40deg,
+    hsl(267deg 100% 7%) 0%,
+    hsl(298deg 69% 14%) 6%,
+    hsl(323deg 100% 23%) 13%,
+    hsl(338deg 100% 33%) 19%,
+    hsl(0deg 72% 48%) 25%,
+    hsl(37deg 76% 43%) 31%,
+    hsl(69deg 37% 56%) 37%,
+    hsl(137deg 44% 79%) 44%,
+    hsl(174deg 100% 96%) 50%,
+    hsl(190deg 66% 73%) 56%,
+    hsl(194deg 97% 45%) 63%,
+    hsl(206deg 100% 43%) 69%,
+    hsl(231deg 62% 46%) 75%,
+    hsl(226deg 70% 33%) 81%,
+    hsl(224deg 70% 23%) 87%,
+    hsl(226deg 58% 14%) 94%,
+    hsl(236deg 70% 4%) 100%
+  );
+  width: 100%;
+  height: 400px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.jumbotron h1{
+  text-shadow: 0 0 10px blueviolet;
+}
+</style>
